b1337178f113ab5cfb9954c09912cde75d6e383f
Allison Meikle <allison.meikle@mail.mcgill.ca>
2025-11-20 17:40:55 -0500

    Added domains.csv

--- scripts/get_north_american_sources.py
+++ scripts/get_north_american_sources.py
@@ -8,7 +8,6 @@ BASE_URL = "https://api.thenewsapi.com/v1/news/sources?language=en"
 SCRIPT_DIR = os.path.dirname(__file__)
 
 def main():
-    
     load_dotenv()
     NEWS_API_TOKEN = os.getenv("NEWS_API_TOKEN")
     if not NEWS_API_TOKEN:
@@ -16,18 +15,20 @@ def main():
     
     sources = {"sources":[]}
     page = 1
-    while (page < 500):
-        print(f"Got page {page}")
-        query_url = BASE_URL + f'&api_token={NEWS_API_TOKEN}&page={page}'
+    for i in range(1, 101):
+        print(f"Got page {i}")
+        query_url = BASE_URL + f'&api_token={NEWS_API_TOKEN}&page={i}'
         page += 1
         response = requests.get(query_url)
         response.raise_for_status()
         response_json = json.loads(response.text)
         sources["sources"].extend(response_json['data'])
         
-        
-    with open(os.path.join(SCRIPT_DIR, "..", "data", "sources", "sources.json"), "w") as f:
+    with open(os.path.join(SCRIPT_DIR, "..", "data", "sources.json"), "w") as f:
         json.dump(sources, f)
+    
+    df = pd.read_json(os.path.join(SCRIPT_DIR, "..", "data", "sources.json"))
+    print(len(df))
 
 if __name__ == "__main__":
     main()
